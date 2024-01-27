e1b34379a5c000f272d04d5e7a87d0ce6ea67a86
ganpatlabde <glabde@ymail.com>
2024-01-27 19:40:45 +0530

    initial commit

---
+++ src/Student.java
@@ -0,0 +1,14 @@
+public class Student {
+
+    String name;
+    Integer age;
+    String email;
+    String batchName;
+    Float psp;
+
+    void changeBatch(String newBatchName)
+    {
+        batch = newBatchName;
+
+    }
+}
